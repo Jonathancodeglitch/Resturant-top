cddf7b81c17191c33854c3d824f023b9f0ce2c98
Jonathan <jonathankendrick697@gmail.com>
2023-09-24 23:45:55 -0700

    Finished hard coding the UI

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import './style.css';
 import toggleNav from './components/nav_toggle/nav_toggle';
-//import the initail page load funvtion from another module
+//import the initial page load funvtion from another module
 //set up your project to use tab browsing to navigate between home,menu,contacts
 //put the content of each tab inside its own module
 //write the tab switching logic inside of the app.js
